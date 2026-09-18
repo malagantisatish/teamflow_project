f29fb5b728ddb6b1b515c6b7529f8a09e965c987
malagantisatish <satishvarma8247@gmail.com>
2026-09-18 07:03:56 +0530

    added auth controller and auth service for login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { loginQuery } from "../services/auth.service";
 
 export const login = async (req: Request, res: Response) => {
 
@@ -10,6 +11,29 @@ export const login = async (req: Request, res: Response) => {
                 message: "Email and password are required"
             })
         }
+
+        const result = await loginQuery({ email: email, password: password })
+
+        if (result.error === "EMAIL_NOT_FOUND") {
+            return res.status(401).json({
+                status: "error",
+                message: "Email not found"
+            })
+        }
+        if (result.error === "INVALID_PASSWORD") {
+            return res.status(401).json({
+                status: "error",
+                message: "Incorrect password"
+            })
+        }
+
+
+        return res.status(200).json({
+            status: "success",
+            message: "Login successfull",
+            user: result
+        })
+
 
     } catch (error: any) {
         console.log(error)
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,40 @@
-export const loginQuery = ({ email, password }: { email: string, password: string }) => {
+import bcrypt from "bcrypt";
+import { pool } from "../db/pool";
+
+export const loginQuery = async ({ email, password }: { email: string, password: string }) => {
+
+    const result = await pool.query(`
+        SELECT * 
+        FROM users
+        WHERE email=$1`, [email])
+
+    if (result.rows.length === 0) {
+        return {
+            error: "EMAIL_NOT_FOUND"
+        };
+    }
+
+    const user = result.rows[0];
+
+    const isPasswordValid = await bcrypt.compare(
+        password, user.password_hash
+    )
+
+    if (!isPasswordValid) {
+        return {
+            error: "INVALID_PASSWORD"
+        };
+    }
+
+    return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+
+    }
+
+
+
+
 
 }
